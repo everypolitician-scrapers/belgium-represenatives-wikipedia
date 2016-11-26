ee5f2cd7ad59330f0d5f810a72ca69a9f7335a4d
Tony Bowden <tony@tmtm.com>
2016-11-26 19:16:12 +0000

    Add special case for Vincent Van Peteghem
    This is another where the notes are a little too hard to parse because
    of a double replacement.
    
    It's also easier to follow what's going on if these are all inlined into
    the relevant section rather than pre-defined earlier.

--- scraper.rb
+++ scraper.rb
@@ -22,11 +22,6 @@ def date_from(str)
   return "%d-%02d-%02d" % [y, @MONTHS.find_index(m), d] rescue abort "Unknown month: #{m}"
 end
 
-# Hard-code some hard-to-parse mid-term changes
-WILMES =   { name: 'Sophie Wilmès', wikiname__nl: 'Sophie Wilmès', start_date: '2014-10-11', end_date: '2015-09-22' }
-REYNDERS = { name: 'Didier Reynders', wikiname__nl: 'Didier Reynders', end_date: '2014-10-11' }
-WILRYCX =  { name: 'Frank Wilrycx', wikiname__nl: 'Frank Wilrycx', start_date: '2014-07-30', end_date: '2016-04-29' }
-
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
@@ -47,19 +42,35 @@ def scrape_list(url)
       start_date: '2014-06-19',
     }
 
+    # Temporary replacements
     if tds[4] && !(notes = tds[4].text.tidy).empty?
-      if notes.include? 'vervangt vanaf 22 september 2015 Sophie Wilmès, die minister wordt in de federale regering-Michel. Wilmès zelf verving vanaf 11 oktober 2014 Didier Reynders, die ook minister in de regering-Michel werd'
-        wilmes = data.merge(WILMES)
-        reynders = data.merge(REYNDERS)
+      # Gautier Calomne replacing Sophie Wilmès replacing Didier Reynders
+      if notes.include? 'vervangt vanaf 22 september 2015 Sophie Wilmès, die minister wordt in de federale regering-Michel. Wilmès zelf verving vanaf 11 oktober 2014 Didier Reynders'
+        wilmes   = { name: 'Sophie Wilmès', wikiname__nl: 'Sophie Wilmès', start_date: '2014-10-11', end_date: '2015-09-22' }
+        reynders = { name: 'Didier Reynders', wikiname__nl: 'Didier Reynders', end_date: '2014-10-11' }
+        wilmes_m = data.merge(wilmes)
+        reyn_m   = data.merge(reynders)
         data[:start_date] = wilmes[:end_date]
-        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], wilmes)
-        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], reynders)
+        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], wilmes_m)
+        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], reyn_m)
 
+      # Vincent Van Peteghem replacing Sarah Claerhout replacing Pieter De Crem
+      elsif notes.include? 'vervangt vanaf 10 november 2016 Sarah Claerhout, die uit CD&V stapt en haar zetel aan haar partij teruggeeft. Claerhout zelf verving 14 oktober 2014 Pieter De Crem'
+        claerhout = { name: 'Sarah Claerhout', wikiname__nl: 'Sarah Claerhout', start_date: '2014-10-14', end_date: '2016-11-10' }
+        decrem    = { name: 'Pieter De Crem', wikiname__nl: 'Pieter De Crem', end_date: '2014-10-14' }
+        claer_m   = data.merge(claerhout)
+        decrem_m  = data.merge(decrem)
+        data[:start_date] = claerhout[:end_date]
+        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], claer_m)
+        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], decrem_m)
+
+      # Annemie Turtelboom temporarily replaced by Frank Wilrycx
       elsif notes.include? 'werd van 30 juli 2014 tot 29 april 2016 als minister in de Vlaamse regering-Bourgeois vervangen door Frank Wilrycx'
-        wilryxc = data.merge(WILRYCX)
-        turtelboom_first = data.clone.merge(end_date: wilryxc[:start_date])
-        data[:start_date] = wilryxc[:end_date]
-        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], wilryxc)
+        wilrycx = { name: 'Frank Wilrycx', wikiname__nl: 'Frank Wilrycx', start_date: '2014-07-30', end_date: '2016-04-29' }
+        wil_m   = data.merge(wilrycx)
+        turtelboom_first = data.clone.merge(end_date: wilrycx[:start_date])
+        data[:start_date] = wilrycx[:end_date]
+        ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], wil_m)
         ScraperWiki.save_sqlite([:wikiname__nl, :term, :start_date], turtelboom_first)
 
       elsif notes.include?('vervangt vanaf')
